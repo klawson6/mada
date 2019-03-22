eb72463f2368f7457e4bd87be0c48d0f8303c08d
Michael Davie CSM2016 <michael.davie.2016@uni.strath.ac.uk>
2019-03-22 16:49:45 +0000

    Utilities.php with dbConn

---
+++ review.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: micha
+ * Date: 21/03/2019
+ * Time: 14:36
+ */
+include "utilities.php";
+
+if (isset($_SESSION['email'])){
+    $userEmail = $_SESSION['email'];
+}
+
+$dbconn = dbconn();
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+    <meta name="mobile-web-app-capable" content="yes" />
+    <meta name="apple-mobile-web-app-capable" content="yes" />
+
+    <title>Co-Rider</title>
+
+    <link rel="icon" sizes="192x192" href="img/car.png" />
+    <link rel="apple-touch-icon" href="img/car.png" />
+    <link rel="shortcut icon" href="img/car.png" type="image/x-icon" />
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="Normalize.css"/>
+    <link rel="stylesheet" type="text/css" href="StyleSheet.css"/>
+
+</head>
+<body>
+<b>Review</b>
+<textarea id="reviewInput" name="reviewInput" placeholder="<?php echo $loggedIn ? "Write your review here..." : "Sign in to leave a review..."?>" style="height:200px" <?php echo $loggedIn ? "" : "disabled"?>></textarea>
+
+<div class="form-group">
+
+</body>
+
+<footer>
+
+    <!-- JQuery Plugins -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+</footer>
+</html>
