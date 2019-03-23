00e04ca0a9b5851a34da369ddee86d4bb22794ef
Nellie Oliver <jjb16181@uni.strath.ac.uk>
2019-03-23 15:08:54 +0000

    added swiping elements, start

--- Home.jsx
+++ Home.jsx
@@ -1,5 +1,43 @@
-//import React from "react";
+/*------------------------------------------------------------------------------------------------------------------------------------------------------*/
+/*                                                                                  Swiping*/
 
+var xFirst, yFirst;
+
+/* get first touch location*/
+function startTouchEvent(evt){
+    const firstTouch = evt.touches || evt.originalEvent.touches;
+    xFirst = firstTouch[0].clientX;
+    yFirst = firstTouch[0].clientY;
+}
+
+
+/*get where exactly they are swiping*/
+function getTouchOrientation(evt) {
+    /*if the either of the input is empty*/
+    if(!xFirst || !yFirst){
+        return;
+    }
+    const xSecond = evt.touches[0].clientX, ySecond = evt.touches[1].clientY;
+    const xSwipe = xFirst - xSecond, ySwipe = yFirst - ySecond;
+
+    if(Math.abs(xSwipe)>Math.abs(ySwipe)){
+        if ( xSwipe > 0 ) {
+            /* left swipe */
+            console.log("left");
+        } else {
+            /* right swipe */
+            console.log("right");
+        }
+    }
+};
+
+/* document listener */
+document.addEventListener("touchstart", startTouchEvent, false);
+document.addEventListener("touchmove", getTouchOrientation, false);
+
+
+/*------------------------------------------------------------------------------------------------------------------------------------------------------*/
+/*                                                          React Components*/
 class UserProfilePage extends React.Component {
     render() {
         return(
@@ -12,6 +50,6 @@ class UserProfilePage extends React.Component {
     }
 }
     ReactDOM.render(
-    <UserProfilePage link = "http://alexmcarthur.co.uk/biggie.png" alt = "Happy Man" data = "David Smith | 27"/>,
+    <UserProfilePage link = "img/woman.jpeg" alt = "Happy Man" data = "David Smith | 27"/>,
     document.getElementById('root')
 );
