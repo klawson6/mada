443dd0aa49b2c8552485af2dc045ba0d256afcd4
Christopher Withers <christopher.withers.2016@uni.strath.ac.uk>
2019-03-29 00:48:04 +0000

    button looks better

--- review.php
+++ review.php
@@ -214,12 +214,12 @@ if (isset($_POST['submit'])){
     event.preventDefault();
     var xhttp = new XMLHttpRequest();
 
-    xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-        console.log(this.responseText);
-        console.log(JSON.parse(this.responseText));
-    }
-    };
+        xhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                console.log(this.responseText);
+                console.log(JSON.parse(this.responseText));
+            }
+        };
     xhttp.open("POST", "review.php", true);
     xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
     xhttp.send("submit=true&c=" + cleanliness + "&d=" + driving + "&p=" + personality + "&t=" + timeliness);
